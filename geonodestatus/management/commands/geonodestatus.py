86339c7107ee4439867f4f7422e9012c4ae303a5
Jude Mwenda <judemwenda@gmail.com>
2011-11-22 17:01:16 -0500

    minor changes and closing the issue.Closes #3

--- geonodestatus/management/commands/geonodestatus.py
+++ geonodestatus/management/commands/geonodestatus.py
@@ -1,7 +1,7 @@
 #means to know layers and maps that are troublesome#means to know layers and maps that are troublesome#means to know layers and maps that are troublesome
 from django.core.management.base import BaseCommand,CommandError
 from geonode.maps.models import Map, Layer, MapLayer
-from geonode.geonodestate.models import Badlayers,BadMaps
+from geonode.geonodestate.models import Badlayers
 from django.shortcuts import render_to_response, get_object_or_404
 import urllib2
 from datetime import datetime
