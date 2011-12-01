27050885bb16f939162f3002b6bd15b2ec5aeaba
Jude Mwenda <judemwenda@gmail.com>
2011-12-01 10:55:23 -0500

    Fixed list_display in admin.py

--- monitor/admin.py
+++ monitor/admin.py
@@ -2,7 +2,7 @@ from monitor.models import FaultyLayer
 from django.contrib import admin
 
 class FaultyLayerAdmin(admin.ModelAdmin):
-    list_display = ['layer', 'error_code']
+    list_display = ['layer_name', 'error_code']
 
 admin.site.register(FaultyLayer, FaultyLayerAdmin)
 
