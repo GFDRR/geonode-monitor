e276164f755bdf1d59af601a2748c1f8cae1e903
Jude Mwenda <judemwenda@gmail.com>
2011-12-12 10:13:48 -0500

    fixed the checkdate to have the same date

--- monitor/management/commands/faultylayers.py
+++ monitor/management/commands/faultylayers.py
@@ -3,70 +3,102 @@ from django.core.management.base import BaseCommand
 from django.template.loader import render_to_string
 from django.core.mail import mail_admins
 from django.conf import settings
+from django.utils import simplejson
 from geonode.maps.models import Map, Layer
 from monitor.models import FaultyLayer
+from xml.dom.minidom import parseString
 import httplib
 import urllib2,urllib
-import simplejson
+import datetime
 import os
 
 
-def get_faulty_maps():
+def get_faulty_maps(check_date):
     """Gets a list of maps failing based on the FaultyLayer table.
 
        If a faulty layer is contained in a map it is marked as invalid.
        If two faulty layers are present in a map, this will return only one entry.
     """
     map_ids = []
-    for layer in FaultyLayer.objects.all():
+    for layer in FaultyLayer.objects.filter(check_date=check_date):
         map_ids.extend([themap.id for themap in layer.layer.maps()])
     # use the list of ids (possibly repeated) to filter out map objetcs.
     faulty_maps = Map.objects.filter(id__in=map_ids).distinct()
     return faulty_maps
-
-
+    
+    
+def get_url(host, path, headers={}):
+    """Gets the content or status code of a url.
+       If there is an error connecting, it returns -1 as the status code.
+        
+       Output: Dictionary with status code, response and reason.
+    """
+    conn = httplib.HTTPConnection(host,timeout=10)
+    conn.request("GET",path, None,headers)
+    try:
+       response = conn.getresponse()
+       status_code = response.status
+       contenttype = response.getheader('Content-Type')
+       conn.close()
+       reason = response.read()
+       return status_code, contenttype, reason
+    except Exception, e:
+       return -1, None, str(e)
 def check_layers():
     """Tries to connect to the layer url and creates a FaultyLayer object if it can not do it.
     """
     url = urllib2.urlparse.urlsplit(settings.GEOSERVER_BASE_URL)[1]
     geonodeurl = urllib2.urlparse.urlsplit(settings.SITEURL)[1]
+    check_date = datetime.datetime.now()
     for layer in Layer.objects.all():
-        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-        conn = httplib.HTTPConnection(url,timeout=10)
-        conn2 = httplib.HTTPConnection(geonodeurl,timeout=10)
-        wmslink = '/geoserver/wms/reflect?layers=' + layer.typename
-        detaillink = '/data/'+layer.typename
-        conn.request("GET",wmslink,"",headers)
-        conn2.request("GET", layer.get_absolute_url())
-        r1 = conn.getresponse()
-        status_code = r1.status
-        r2 = conn2.getresponse()
-        code = r2.status
-        contenttype = r1.getheader('Content-Type')
-        reason = r1.read()
+        detail_code, detail_contenttype, detail_reason = get_url(geonodeurl,
+                                                                 layer.get_absolute_url())
+        # Check if the detail page works (401 is excluded too)
+        detail_page_fails  = detail_code not in [200, 401]
+
+        # Check for pink layers.
         perm = layer.get_all_level_info()
-        if 'anonymous' not in  perm:
-            reason = 'No permissions to query this'
-        print '%s [%s]' % (wmslink,status_code)
-        print '%s [%s]' % (detaillink,code)
+        if 'anonymous' in  perm:
+            headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+            wmslink = '/geoserver/wms/reflect?layers=' + layer.typename
+            wms_code, wms_contenttype, wms_reason = get_url(url, wmslink, headers)
+            pink_tile = wms_contenttype != 'image/png'
+        else:
+            # Avoid checking the protected ones for pink tiles.
+            pink_tile = False
+
+        if detail_page_fails:
+            #FIXME: Extract title from XML if possible, if not, store the whole document.
+            title = None
+#           dom = parseString(detail_reason)
+#           title = dom.getElementsByTagName("title")[0]
+            if title is not None:
+                reason = title
+            else:
+                reason = detail_reason
+        elif pink_tile:
+            reason = 'The layer is not rendering: %s' % wms_reason
 
         # Only layers without an image content type are deemed to be irresponsible
-        if contenttype != 'image/png' or code not in [200, 401]:
-            FaultyLayer.objects.create(layer=layer,error_code=status_code, reason=reason)
-        conn.close()
+        if pink_tile or detail_page_fails:
+            print '%s (failed) http://%s%s' % (layer.name,
+                                   geonodeurl, layer.get_absolute_url())
+            FaultyLayer.objects.create(layer=layer,error_code=detail_code,
+                                       reason=reason, check_date=check_date)
+        else:
+            print '%s (ok)'  % layer.name
 
-    context = {} 
+    context = {}
     context["name"] = settings.SITENAME
     context['url'] = settings.SITEURL
     context["geoserver_base_url"] = settings.GEOSERVER_BASE_URL
     context["geonetwork_base_url"] = settings.GEONETWORK_BASE_URL
     context["layer_count"] =  Layer.objects.count()
     context["map_count"] = Map.objects.count()
-    context["faulty_layers"] = FaultyLayer.objects.all()
-    context["faulty_maps"] = get_faulty_maps()
+    context["faulty_layers"] = FaultyLayer.objects.filter(check_date=check_date)
+    context["faulty_maps"] = get_faulty_maps(check_date)
     context["backup_date"] = backupdate()
-    return context     
-
+    return context
 
 def send_admin_email(context):
     """This is the function that sends the site administrators an email on faulty layer and faulty maps"""
@@ -89,7 +121,6 @@ def backupdate():
     except:
         t = None
     return t
-
 
 def send_registry(context):
     """This function deals pushing logs/data to the geonode registry
@@ -121,5 +152,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **keywordargs):
         context = check_layers()
-        send_admin_email(context) 
+        send_admin_email(context)
         send_registry(context)
--- monitor/models.py
+++ monitor/models.py
@@ -5,7 +5,7 @@ from geonode.maps.models import Layer
 class FaultyLayer(models.Model):
     layer = models.ForeignKey(Layer)
     error_code = models.IntegerField()
-    check_date = models.DateTimeField(auto_now_add=True, help_text="Date the layer was known to have problems.")
+    check_date = models.DateTimeField(help_text="Date the layer was known to have problems.")
     reason = models.TextField(null=True, blank=True, help_text="Longer description about the error, if possible with a traceback.")
 
     def affected_maps(self):
